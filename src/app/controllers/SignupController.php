2487dd924d261e7fa940b4970685935e860d8b00
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-04-21 19:03:23 +0530

    second commit

--- src/app/controllers/SignupController.php
+++ src/app/controllers/SignupController.php
@@ -2,6 +2,11 @@
 
 use Phalcon\Mvc\Controller;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+
+
 class SignupController extends Controller{
 
     public function IndexAction()
@@ -14,9 +19,26 @@ class SignupController extends Controller{
         $name = $this->request->get('name');
         $email = $this->request->get('email');
         $password = $this->request->get('password');
-        $success = $signup->insertOne(['name' => $name, 'email' => $email , 'password' => $password]);
-
-
-       die;
+        $role = 'user';
+        $success = $signup->insertOne(['name' => $name, 'email' => $email , 'password' => $password, 'role' => $role]);
+        $token = $this->token($name, $email);
+        echo $token;
+        die;
     }
+     public function token($name, $email) {
+    $key = "example_key";
+    $payload = array(
+        "iss" => "http://example.org",
+        "aud" => "http://example.com",
+        "iat" => 1356999524,
+        "exp" => time() * 24 + 3600,
+        "role" => "user",
+        "name" => $name,
+        "email" => $email
+    );
+
+    $jwt = JWT::encode($payload, $key, 'HS256');
+
+    return $jwt;
+}
 }
